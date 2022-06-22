f944face8f520b6c5199fcffefa85468ad362cac
alexanderbazo <alexanderbazo@googlemail.com>
2022-06-22 08:13:09 +0200

    Initial commit

---
+++ resources/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * To display a card, create a <span>-Element in #board. Use one of these css classes to determine the
+ * card's face ("skunk","duck","tortoise","whale","bee","parrot","rat","kangaroo"). A card's visible state 
+ * is determined by adding one of these additional classed "closed" (card's face is not visible), "open" 
+ * (card's face is visible) and "solved" (card's face is visible with lowered opacity).
+ */
+
+const AVAILABLE_CARD_FACES = ["skunk","duck","tortoise","whale","bee","parrot","rat","kangaroo"];
+
+function init() {
+  // eslint-disable-next-line no-console
+  console.log("#### Starting Memoji ####");
+  // TODO: Start your implementation here
+}
+
+init();
